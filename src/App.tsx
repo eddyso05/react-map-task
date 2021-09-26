9b4e4c9a34b9e3fd7f6d42e45e9be7cdaeebad77
eddy siow <eddysiow0530@hotmail.com>
2021-09-27 00:19:34 +0800

    Feat: Refresh Interval on Map

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
+import { useEffect } from "react";
 import Box from "@mui/material/Box";
 import LoadingOverlay from "react-loading-overlay";
-import { useSelector, RootStateOrAny } from "react-redux";
+import { useSelector, RootStateOrAny, useDispatch } from "react-redux";
 
 import Map from "./components/Map";
 import NavBar from "./components/BaseLayout/NavBar";
 import Slider from "./components/Slider";
 import Modal from "./components/Modal";
+import { fetchDrivers } from "./features/drivers/services";
 
 function App() {
+  const dispatch = useDispatch();
   const loading = useSelector(
     (state: RootStateOrAny) => state.root.map.loading
   );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      dispatch(fetchDrivers(1));
+    }, 60000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [dispatch]);
 
   return (
     <div className="App">
