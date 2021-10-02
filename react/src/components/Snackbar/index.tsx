5d99f20a967372e91dbaa986a283f539ec26329d
eddy siow <eddysiow0530@hotmail.com>
2021-10-02 13:21:58 +0800

    Fix: remove snackbar

--- react/src/components/Snackbar/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { SnackbarProvider } from "notistack";
-
-export const Snackbar = (children: React.ReactNode) => {
-  return <SnackbarProvider children={children} />;
-};
-
-export default Snackbar;
